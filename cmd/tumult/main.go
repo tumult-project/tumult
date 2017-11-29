844b365dd072ae219dec079bd67951b2d8d85beb
Raül Pérez <repejota@gmail.com>
2017-11-29 17:40:36 +0100

    Basic cli
    * Custom usage method and message help
    * Global -help flag
    * Global -version flag

--- cmd/tumult/main.go
+++ cmd/tumult/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s [-version] [-help] <command> [<args>]\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "Available commands are:\n")
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "Available flags are:\n")
+	fmt.Fprintf(os.Stderr, "  -help\n")
+	fmt.Fprintf(os.Stderr, "\tShow this help message\n")
+	flag.PrintDefaults()
+}
+
+func version() {
+	version := "v0.0.0"
+	fmt.Printf("%s %s\n", os.Args[0], version)
+}
 
 func main() {
-	fmt.Println("tumult entry point.")
+	versionPtr := flag.Bool("version", false, "Show version information")
+
+	flag.Usage = usage
+	flag.Parse()
+
+	if *versionPtr {
+		version()
+		os.Exit(0)
+	}
+
+	usage()
 }
